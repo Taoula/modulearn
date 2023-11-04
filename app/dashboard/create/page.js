7e4a292037b60a406adaf555f891e64587f673b3
Paul Chinnam <63817253+paulchinnam@users.noreply.github.com>
2023-11-04 16:32:07 -0500

    fddsf

--- app/dashboard/create/page.js
+++ app/dashboard/create/page.js
@@ -1,29 +1,17 @@
 "use client";
 
-import { useRouter } from "next/navigation";
-import { useAuth } from "../../hooks/useFirebase";
 import { PlusCircleIcon } from "@heroicons/react/20/solid";
 import { useState } from "react";
 import CreateNewLesson from "../../components/CreateNewLesson";
 import CreateNewRoadmap from "../../components/CreateNewRoadmap";
 
 export default function Page() {
-  const { signOut } = useAuth();
-  const router = useRouter();
   const [createPanelOpen, setCreatePanelOpen] = useState(false);
   const [createNewLessonOpen, setCreateNewLessonOpen] = useState(false);
   const [createNewRoadmapOpen, setCreateNewRoadmapOpen] = useState(false);
 
-  const logOut = async function () {
-    await signOut().then(() => {
-      router.push("/");
-    });
-  };
-
   return (
     <>
-      <h1>Dashboard</h1>
-      <button onClick={logOut}>Sign Out</button>
       <PlusCircleIcon
         className="h-5 w-5"
         onClick={() => setCreatePanelOpen((prev) => !prev)}
