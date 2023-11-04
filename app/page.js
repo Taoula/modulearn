913d10d3ed525c6146fdddbe2cb34b522efc2cad
Paul Chinnam <63817253+paulchinnam@users.noreply.github.com>
2023-11-04 15:55:50 -0500

    fsdfsdfsfds

--- app/page.js
+++ app/page.js
@@ -2,12 +2,14 @@
 
 import Image from "next/image";
 import SignUp from "./components/SignUp";
+import image from "public/home.jpg";
 
 export default function Home() {
   return (
     <>
       <div className="flex items-center h-screen">
         <div className="mx-auto">
+          <Image src={image} className="h-72 w-auto mb-8" />
           <h1 className="text-center text-4xl font-semibold pb-6">Modulearn</h1>
           <SignUp />
         </div>
