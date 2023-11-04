2a91510c3b7a3936c081a15f357077620de409df
Ryan McCord <mccordryan00@gmail.com>
2023-11-04 15:34:25 -0500

    create

--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -2,10 +2,13 @@
 
 import { useRouter } from "next/navigation";
 import { useAuth } from "../hooks/useFirebase";
+import { PlusCircleIcon } from "@heroicons/react/20/solid";
+import { useState } from "react";
 
 export default function Page() {
   const { signOut } = useAuth();
   const router = useRouter();
+  const [createPanelOpen, setCreatePanelOpen] = useState(false);
 
   const logOut = async function () {
     await signOut().then(() => {
@@ -16,6 +19,16 @@ export default function Page() {
     <>
       <h1>Dashboard</h1>
       <button onClick={logOut}>Sign Out</button>
+      <PlusCircleIcon
+        className="h-5 w-5"
+        onClick={() => setCreatePanelOpen((prev) => !prev)}
+      />
+      {createPanelOpen && (
+        <div>
+          <button>New Lesson</button>
+          <button>New Roadmap</button>
+        </div>
+      )}
     </>
   );
 }
