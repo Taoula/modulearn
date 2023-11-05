5ba77fd9e2c097ad417e37aa4dabc90d2477df6a
Paul Chinnam <63817253+paulchinnam@users.noreply.github.com>
2023-11-04 22:25:15 -0500

    vcxvcx

--- app/dashboard/learn/page.js
+++ app/dashboard/learn/page.js
@@ -22,21 +22,39 @@ export default function Page() {
         <h1 className="text-3xl font-bold">{data?.title}</h1>
         <div className="flex justify-between items-center">
           {index != 0 && (
-            <div className="p-2 rounded-full bg-blue-500 hover:bg-blue-600 duration-150 h-fit w-fit hover:cursor-pointer hover:scale-110">
-              <ChevronLeftIcon
-                className="h-10 w-10 text-white"
-                onClick={() => setIndex((prev) => prev - 1)}
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="w-10 h-10 hover:stroke-green-600 hover:fill-white duration-200"
+              onClick={() => setIndex((prev) => prev - 1)}
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M11.25 9l-3 3m0 0l3 3m-3-3h7.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
               />
-            </div>
+            </svg>
           )}
           {data && <LessonPage pageData={data?.pages[index]} />}
           {index != data?.pages?.length - 1 && (
-            <div className="p-2 rounded-full bg-blue-500 hover:bg-blue-600 duration-150 h-fit w-fit hover:cursor-pointer hover:scale-110">
-              <ChevronRightIcon
-                className="h-10 w-10 text-white"
-                onClick={() => setIndex((prev) => prev + 1)}
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="w-10 h-10 hover:stroke-green-600 hover:fill-white duration-200"
+              onClick={() => setIndex((prev) => prev + 1)}
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M12.75 15l3-3m0 0l-3-3m3 3h-7.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
               />
-            </div>
+            </svg>
           )}
         </div>
       </div>
