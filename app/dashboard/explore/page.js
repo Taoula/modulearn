3d3c242d1c3e79d946248cb3eae4008466d92104
Manu Shanbhog <57578422+shanboss@users.noreply.github.com>
2023-11-05 06:12:15 -0600

    search bar css

--- app/dashboard/explore/page.js
+++ app/dashboard/explore/page.js
@@ -3,6 +3,7 @@ import React, { useState } from "react";
 import algoliasearch from "algoliasearch/lite";
 import { InstantSearch, SearchBox, Hits } from "react-instantsearch";
 import SearchResult from "@/app/components/SearchResult";
+import { QuestionMarkCircleIcon } from "@heroicons/react/20/solid";
 
 function ExplorePage() {
   const searchClient = algoliasearch(
@@ -14,7 +15,17 @@ function ExplorePage() {
     <div className="mt-12">
       <div>
         <InstantSearch searchClient={searchClient} indexName="ModulearnTitles">
-          <SearchBox />
+          <SearchBox
+            placeholder="Learn something new..."
+            classNames={{
+              root: "flex justify-center pb-10",
+              form: "flex justify-center w-full ",
+              input:
+                "flex justify-center px-3 w-4/5 shadow-sm block w-full rounded-md border-0  text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6",
+              submit:
+                "flex justify-center shadow-sm block px-5 py-3 bg-green-200 rounded-md border-0  text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 hover:bg-green-500 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6",
+            }}
+          />
           <Hits
             hitComponent={({ hit }) => <SearchResult id={hit.objectID} />}
           />
