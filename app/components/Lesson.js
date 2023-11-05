06fadf933f515b0d38073d46d5d3a8dc41456be3
Ryan McCord <mccordryan00@gmail.com>
2023-11-04 21:09:52 -0500

    lessons page

--- app/components/Lesson.js
+++ app/components/Lesson.js
@@ -1,6 +1,10 @@
-export default function Lesson({ lesson }) {
-  <div className="bg-white p-5 shadow-md rounded-lg m-3">
-    <h1>Lesson Name</h1>
-    <button>Start Lesson</button>
-  </div>;
+export default function Lesson({ lessonData }) {
+  return (
+    <div className="bg-white p-5 shadow-md rounded-lg m-3 text-black w-fit flex flex-col justify-center gap-3">
+      <h1 className="font-semibold text-lg">{lessonData?.title}</h1>
+      <button className="w-full bg-green-500 text-white py-1 px-2 rounded-md hover:bg-green-600 duration-150 font-semibold text-lg">
+        Start Lesson
+      </button>
+    </div>
+  );
 }
--- app/dashboard/lessons/page.js
+++ app/dashboard/lessons/page.js
@@ -1,3 +1,23 @@
+"use client";
+
+import Lesson from "@/app/components/Lesson";
+import { useCollection } from "@/app/hooks/useFirebase";
+import { useEffect } from "react";
+
 export default function Page() {
-  return <></>;
+  const { data } = useCollection("lessons");
+
+  useEffect(() => {
+    console.log(data);
+  }, [data]);
+  return (
+    <>
+      <div>
+        {data &&
+          data.map((lesson) => {
+            return <Lesson lessonData={lesson} />;
+          })}
+      </div>
+    </>
+  );
 }
