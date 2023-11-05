44ae23236e7c0996194b8cfc539396c51e1f07af
Paul Chinnam <63817253+paulchinnam@users.noreply.github.com>
2023-11-04 21:49:14 -0500

    fdsfsd

--- app/components/Lesson.js
+++ app/components/Lesson.js
@@ -1,19 +1,55 @@
 import { useRouter } from "next/navigation";
 
-export default function Lesson({ lessonData }) {
+export default function Lesson({ lessonData, key }) {
   const router = useRouter();
 
+  function classNames(...classes) {
+    return classes.filter(Boolean).join(" ");
+  }
+
   return (
-    <div className="bg-white p-5 shadow-md rounded-lg m-3 text-black w-fit flex flex-col justify-center gap-3">
-      <h1 className="font-semibold text-lg">{lessonData?.title}</h1>
-      <button
-        className="w-full bg-green-500 text-white py-1 px-2 rounded-md hover:bg-green-600 duration-150 font-semibold text-lg"
-        onClick={() =>
-          router.push(`/dashboard/learn?lessonId=${lessonData?.id}`)
-        }
+    // <div className="bg-white p-5 shadow-md rounded-lg m-3 text-black w-fit flex flex-col justify-center gap-3">
+    //   <h1 className="font-semibold text-lg">{lessonData?.title}</h1>
+    //   <button
+    //     className="w-full bg-green-500 text-white py-1 px-2 rounded-md hover:bg-green-600 duration-150 font-semibold text-lg"
+    // onClick={() =>
+    //   router.push(`/dashboard/learn?lessonId=${lessonData?.id}`)
+    // }
+    //   >
+    //     Start Lesson
+    //   </button>
+    // </div>
+    <li key={key} className="col-span-1 flex rounded-md shadow-sm">
+      <div
+        // className={classNames(
+        //   project.bgColor,
+        //   "flex w-16 flex-shrink-0 items-center justify-center rounded-l-md text-sm font-medium text-white"
+        // )}
+        className="bg-green-600 flex w-16 flex-shrink-0 items-center justify-center rounded-l-md text-sm font-medium text-white"
       >
-        Start Lesson
-      </button>
-    </div>
+        P
+      </div>
+      <div className="flex flex-1 items-center justify-between truncate rounded-r-md border-b border-r border-t border-gray-200 bg-white">
+        <div className="flex-1 truncate px-4 py-2 text-sm">
+          <a
+            // href={project.href}
+            className="font-medium text-gray-900 hover:text-gray-600"
+          >
+            {lessonData?.title}
+          </a>
+          <p className="text-gray-500">In progress</p>
+        </div>
+        <div className="flex-shrink-0 pr-4">
+          <button
+            onClick={() =>
+              router.push(`/dashboard/learn?lessonId=${lessonData?.id}`)
+            }
+            className="text-sm px-4 py-1 border rounded-md text-gray-500 hover:bg-green-50 hover:text-green-600 hover:border-green-600 duration-200"
+          >
+            Start
+          </button>
+        </div>
+      </div>
+    </li>
   );
 }
--- app/dashboard/lessons/page.js
+++ app/dashboard/lessons/page.js
@@ -1,5 +1,30 @@
+// "use client";
+
+// import Lesson from "@/app/components/Lesson";
+// import { useCollection } from "@/app/hooks/useFirebase";
+// import { useEffect } from "react";
+
+// export default function Page() {
+//   const { data } = useCollection("lessons");
+
+// useEffect(() => {
+//   console.log(data);
+// }, [data]);
+//   return (
+//     <>
+//       <div>
+// {data &&
+//   data.map((lesson) => {
+//     return <Lesson lessonData={lesson} />;
+//   })}
+//       </div>
+//     </>
+//   );
+// }
+
 "use client";
 
+// import { EllipsisVerticalIcon } from "@heroicons/react/20/solid";
 import Lesson from "@/app/components/Lesson";
 import { useCollection } from "@/app/hooks/useFirebase";
 import { useEffect } from "react";
@@ -7,17 +32,25 @@ import { useEffect } from "react";
 export default function Page() {
   const { data } = useCollection("lessons");
 
+  function classNames(...classes) {
+    return classes.filter(Boolean).join(" ");
+  }
+
   useEffect(() => {
     console.log(data);
   }, [data]);
+
   return (
-    <>
-      <div>
+    <div className="mt-12">
+      <ul
+        role="list"
+        className="mt-3 grid grid-cols-1 gap-5 sm:grid-cols-2 sm:gap-6 lg:grid-cols-3"
+      >
         {data &&
           data.map((lesson) => {
-            return <Lesson lessonData={lesson} />;
+            return <Lesson lessonData={lesson} key={lesson.id} />;
           })}
-      </div>
-    </>
+      </ul>
+    </div>
   );
 }
