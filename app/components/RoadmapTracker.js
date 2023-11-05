2b2204ba52d045d74f47672a4eeb745e39365637
Paul Chinnam <63817253+paulchinnam@users.noreply.github.com>
2023-11-05 01:11:08 -0500

    fdfs

--- app/components/RoadmapTracker.js
+++ app/components/RoadmapTracker.js
@@ -2,7 +2,23 @@ export default function RoadmapTracker({ data }) {
   return (
     <div className="rounded-md border px-6 py-8">
       <div className="pb-8 flex justify-between items-center">
-        <h1 className="text-xl font-medium">{data?.title}</h1>
+        <div className="flex items-center gap-3">
+          <h1 className="text-xl font-medium">{data?.title}</h1>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="w-5 h-5 hover:fill-green-600 hover:stroke-green-600"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z"
+            />
+          </svg>
+        </div>
         <button className="border px-4 rounded-md py-1 text-gray-500 hover:bg-green-50 hover:text-green-600 hover:border-green-600 duration-200">
           Continue
         </button>
